4f58d8d0b5a22e569e7c842b74d35932d7019f5b
unknown <nurlangn@code.edu.az>
2023-07-20 02:41:04 +0400

    added IRepository

---
+++ FiorellaApp/Fiorella/src/Core/Fiorella.Aplication/Abstraction/Repostiory/IWriteRepository.cs
@@ -0,0 +1,19 @@
+﻿using Fiorella.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiorella.Aplication.Abstraction.Repostiory
+{
+    public interface IWriteRepository<T>:IRepository<T> where T : BaseEntity, new()
+    {
+        Task addAsync(T entity);
+        Task addRangeAsync(ICollection<T> enitites);
+        void updateAsync(T entity);
+        void deleteAsync(T entity);
+        Task RemoveAsync(ICollection<T> enitites);
+        Task SaveChangesAsync();
+    }
+}
